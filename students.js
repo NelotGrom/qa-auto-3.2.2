5fa60a1e7019507848168672f075520ca39e7273
Nelot <nelotgrom@gmail.com>
2024-02-05 17:03:19 +0100

    feat: date verifier added (HW 3.2.2)

--- students.js
+++ students.js
@@ -3,23 +3,34 @@ const studentsData = require("./students-data.json");
 let groupNumbrer = 0;
 
 let congraz = function () {
+  const result = [];
+
   for (let group of studentsData) {
     groupNumbrer++;
     score = 0;
+    date = Date.now;
+
     group.forEach((value) => {
-      if (value.score > score) {
+      if (value.score >= score) {
         score = value.score;
+      }
+      if (value.score == score && value.date <= date) {
+        date = value.date;
       }
     });
 
     group.forEach((value) => {
-      if (value.score == score) {
+      if (value.score == score && value.date == date) {
         console.log(
           `Поздравляем ${value.name} с ${value.score} очками из группы №${groupNumbrer}`
         );
+        result.push(value);
       }
     });
   }
+  return result;
 };
+
+congraz();
 
 module.exports = congraz;
--- students.spec.js
+++ students.spec.js
@@ -1,5 +1,9 @@
-let congraz = require('./students.js');
+let congraz = require("./students.js");
 
-test('Smoke test', ()=>{
-    expect(congraz()).toBe(undefined);
-})
+test("Smoke test", () => {
+  expect(congraz()).toStrictEqual([
+    { date: "2022-10-11", name: "Ivan", score: 35 },
+    { date: "2022-10-01", name: "Marina", score: 25 },
+    { date: "2022-10-01", name: "Varvara", score: 0 },
+  ]);
+});
